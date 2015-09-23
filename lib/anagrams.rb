b8e17cdce7b1326239459feaf86952c1d02582f4
Ronando Long <ronando.long@gmail.com>
2015-09-23 16:15:41 -0700

    It will resolve an anagram that matches any word in a multi-word string as well as return false if there is no match. All specs match.

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,21 +1,27 @@
 class String
   define_method(:anagrams) do |check_word|
     words = self.downcase
-    check_word = check_word.downcase
     words_array = words.split(" ")
+    check_word = check_word.downcase
 
     words_array.each do |word|
-      return true if (word.chars.sort) == (check_word.chars.sort)
-      return false if (word.chars.sort) != (check_word.chars.sort)
+      if (word.chars.sort) == (check_word.chars.sort)
+        return true
+      else
+        next
+      end
+    end
+    if (words_array[-1].chars.sort) != (check_word.chars.sort)
+      return false
     end
   end
 end
 
 
 
-# if/else longhand
-    # if (word.chars.sort) == (check_word.chars.sort)
-    #   return true
-    # else
-    #   return false
-    # end
+
+# if/else shorthand
+# words_array.each do |word|
+#   return true if (word.chars.sort) == (check_word.chars.sort)
+#   return false if (word.chars.sort) != (check_word.chars.sort)
+# end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -6,8 +6,8 @@ describe('String#anagrams') do
     expect(("listen").anagrams("silent")).to(eq(true))
   end
 
-  it("will kick back false if argument is not an anagram to the one word object") do
-    expect(("listen").anagrams("cat")).to(eq(false))
+  it("will kick back false if argument is not an anagram to multi-word object") do
+    expect(("listen to dogbert").anagrams("cat")).to(eq(false))
   end
 
   it("will verify if an argument is an anagram when tested against multiple words") do
